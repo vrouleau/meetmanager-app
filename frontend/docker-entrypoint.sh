d59565b99da09ee21a9fb167ac80c9f258081ffd
Vincent Rouleau <vincentrouleau@hotmail.com>
2026-05-12 14:27:30 -0400

    Revert "Add Railway deployment support"
    This reverts commit 1eb28ca98c80c63327ce330c030f4a6714272e75.
    
    Change-Id: Id967cf8d3eb9fb4082fada964159bcc38eedac55

--- frontend/docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e
-# Substitute only ${PORT} — leaves nginx variables ($host, $uri, etc.) untouched
-envsubst '${PORT} ${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-exec nginx -g 'daemon off;'
